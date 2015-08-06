924e551623bf6b79ce1278c3602e9395e4aa6273
ajiang-lee <ajiang_lee@hotmail.com>
2015-08-06 09:17:03 +0800

    添加Nginx相关内容

--- website/src/js/app.js
+++ website/src/js/app.js
@@ -1,7 +1,7 @@
 // 声明应用的主模块，注入angular-ui-router路由模块
 var manual2cn = angular.module('manual2cn', ['ui.router']);
 // 定义路由转换的全局事件
-manual2cn.run(function($rootScope) {
+manual2cn.run(function($rootScope, $location, $anchorScroll) {
     // 当视图开始加载时，执行指定操作
     //$rootScope.$on('$viewContentLoading', function(event, viewConfig) {
     //    $("#globalModal").modal({
@@ -9,6 +9,10 @@ manual2cn.run(function($rootScope) {
     //        keyboard: false
     //    });
     //});
+    $rootScope.gotoAnchor = function(id) {
+        $location.hash(id);
+        $anchorScroll();
+    }
 });
 // 定义路由
 manual2cn.config(function($stateProvider, $urlRouterProvider) {
@@ -36,6 +40,19 @@ manual2cn.config(function($stateProvider, $urlRouterProvider) {
     }).state('home.ui-router-wiki.the-components', {
         url: '/the-components',
         templateUrl: 'partials/angularjs/ui-router/wiki/the-components.html'
+    }).state('home.nginx', {
+        // Nginx的首页路由
+        url: '/nginx',
+        templateUrl: 'partials/nginx/index.html'
+    }).state('home.nginx.installing-nginx', {
+        url: '/installing-nginx',
+        templateUrl: 'partials/nginx/installing-nginx.html'
+    }).state('home.nginx.beginner-guide', {
+        url: '/beginner-guide',
+        templateUrl: 'partials/nginx/beginner-guide.html'
+    }).state('home.nginx.process-request', {
+        url: '/process-request',
+        templateUrl: 'partials/nginx/process-request.html'
     });
 });
 
